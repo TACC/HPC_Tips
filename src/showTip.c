0322a18d0e2a5b494374334f91c02ca13df22fcd
Robert McLay <mclay@zaniahgroup.com>
2014-03-08 13:40:17 -0600

    working version

--- src/showTip.c
+++ src/showTip.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+#include <sys/ioctl.h>
+
 #include "my_global.h"
 #include "mysql.h"
 
@@ -10,15 +16,59 @@ void finish_with_error(MYSQL *con)
   exit(1);        
 }
 
+void printwrap(const char *s, int lineSize, const char *prefix) 
+{
+  const char *head = s;
+  int pos, lastSpace;
+
+  pos = lastSpace = 0;
+
+  while(head[pos]!=0)
+    {
+
+      int isLf = (head[pos]=='\n');
+
+      if (isLf || pos==lineSize)
+        {
+          if (isLf || lastSpace == 0)
+            lastSpace = pos;  // just cut it
+
+          if (prefix!=NULL)
+            printf("%s", prefix); 
+
+          while(*head!=0 && lastSpace-- > 0) 
+            printf("%c", *head++); 
+          printf("\n");
+
+          if (isLf)
+            head++;  // jump the line feed
+
+          while (*head!=0 && *head==' ')
+            head++; // clear the leading space
+
+          lastSpace = pos = 0;
+
+        } 
+      else
+        {
+          if (head[pos]==' ') { lastSpace = pos; }
+          pos++;
+        }
+    }
+  printf("%s\n", head);
+}
+ 
+
 int main(int argc, char **argv)
 {      
-  char        cmd[BUFSIZE];
-  int         i;
-  int         numE = 0;
-  const char* host = "localhost";
-  const char* user = "readerOfTips";
-  const char* pass = "tipReader123";
-  const char* db   = "HPCTips";
+  struct winsize w;
+  char           cmd[BUFSIZE];
+  int            i, twidth;
+  int            numE = 0;
+  const char*    host = "rios.tacc.utexas.edu";
+  const char*    user = "readerOfTips";
+  const char*    pass = "tipReader123";
+  const char*    db   = "HPCTips";
   
 
   MYSQL *con = mysql_init(NULL);
@@ -31,7 +81,7 @@ int main(int argc, char **argv)
   
   /* (1) Open DB */
 
-  if (mysql_real_connect(con, host, user, pass, db, 0, NULL, 0) == NULL) 
+  if (mysql_real_connect(con, host, user, pass, db, 3306, NULL, 0) == NULL) 
     finish_with_error(con);
   
   /* (2) Find number of tips */
@@ -60,9 +110,15 @@ int main(int argc, char **argv)
   srand(time(NULL));
   int idx = rand() % (numE - 1) + 1;
 
+  /* (4) get term width */
+  
+  ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
+  twidth = w.ws_col - 2;
+
+
   /* (4) Select idx tip */
 
-  sprintf(&cmd[0], "select msg from tips where %d", idx);
+  sprintf(&cmd[0], "select msg from tips where tips_id = %d", idx);
 
   if (mysql_query(con, cmd))
     finish_with_error(con);
@@ -75,10 +131,12 @@ int main(int argc, char **argv)
   num_fields = mysql_num_fields(result);
 
   while ((row = mysql_fetch_row(result))) 
-    { 
-      for(i = 0; i < num_fields; i++) 
-	printf("%s ", row[i] ? row[i] : "NULL"); 
-      printf("\n"); 
+    {
+      printf("-------------\n"
+             "Tip : %d\n"
+             "-------------\n\n",idx);
+      printwrap(row[0], twidth, NULL);
+      break;
     }
   
   mysql_free_result(result);
