82c037f4e2c0078a62c6c9d5b4cc889e622e97ad
Robert McLay <mclay@zaniahgroup.com>
2014-03-22 13:38:55 -0500

    setitimer again

--- src/showTip.c
+++ src/showTip.c
@@ -84,7 +84,7 @@ void printUsage(const char* cmd)
 	 cmd);
 }
 
-#define INTERVAL 250000
+#define INTERVAL 400000
 
 int main(int argc, char **argv)
 {      
@@ -112,28 +112,7 @@ int main(int argc, char **argv)
   setitimer(ITIMER_REAL, &timer,0);
 
 
-
-  //struct sigaction sa;
-  //struct itimerval timer;
-  //
-  ///* Install timer_handler as the signal handler for SIGVTALRM. */
-  //memset (&sa, 0, sizeof (sa));
-  //sa.sa_handler = &timer_handler;
-  //sigaction (SIGVTALRM, &sa, NULL);
-  //
-  ///* Configure the timer to expire after 250 msec... */
-  //timer.it_value.tv_sec = 0;
-  //timer.it_value.tv_usec = 250000;
-  ///* ... and every 250 msec after that. */
-  //timer.it_interval.tv_sec = 0;
-  //timer.it_interval.tv_usec = 250000;
-  ///* Start a virtual timer. It counts down whenever this process is
-  //   executing. */
-  //setitimer (ITIMER_VIRTUAL, &timer, NULL);
-
-
   signal(SIGALRM,timer_handler);
-  //alarm(1);
 
   while ( (opt = getopt(argc, argv, "vn:wh?")) != -1)
     {
