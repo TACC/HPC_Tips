150b015400c4cc1d5487aa5d4120f5c4749eaea7
Robert McLay <mclay@zaniahgroup.com>
2014-03-22 11:30:35 -0500

    trying alarm instead

--- src/showTip.c
+++ src/showTip.c
@@ -101,24 +101,29 @@ int main(int argc, char **argv)
   const char*    db   = "HPCTips";
   const char*    hlp  = "\"module help tips\" shows how to disable";
   
-  struct sigaction sa;
-  struct itimerval timer;
 
-  /* Install timer_handler as the signal handler for SIGVTALRM. */
-  memset (&sa, 0, sizeof (sa));
-  sa.sa_handler = &timer_handler;
-  sigaction (SIGVTALRM, &sa, NULL);
 
-  /* Configure the timer to expire after 250 msec... */
-  timer.it_value.tv_sec = 0;
-  timer.it_value.tv_usec = 250000;
-  /* ... and every 250 msec after that. */
-  timer.it_interval.tv_sec = 0;
-  timer.it_interval.tv_usec = 250000;
-  /* Start a virtual timer. It counts down whenever this process is
-     executing. */
-  setitimer (ITIMER_VIRTUAL, &timer, NULL);
-  
+  //struct sigaction sa;
+  //struct itimerval timer;
+  //
+  ///* Install timer_handler as the signal handler for SIGVTALRM. */
+  //memset (&sa, 0, sizeof (sa));
+  //sa.sa_handler = &timer_handler;
+  //sigaction (SIGVTALRM, &sa, NULL);
+  //
+  ///* Configure the timer to expire after 250 msec... */
+  //timer.it_value.tv_sec = 0;
+  //timer.it_value.tv_usec = 250000;
+  ///* ... and every 250 msec after that. */
+  //timer.it_interval.tv_sec = 0;
+  //timer.it_interval.tv_usec = 250000;
+  ///* Start a virtual timer. It counts down whenever this process is
+  //   executing. */
+  //setitimer (ITIMER_VIRTUAL, &timer, NULL);
+
+
+  signal(SIGALRM,timer_handler);
+  alarm(1);
 
   while ( (opt = getopt(argc, argv, "vn:wh?")) != -1)
     {
