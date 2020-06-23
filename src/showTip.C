ee6f5fbecab14f5408b311f9a9d4a671220b4df6
Robert McLay <mclay@zaniahgroup.com>
2020-06-23 17:44:14 -0500

    typo

--- src/showTip.C
+++ src/showTip.C
@@ -175,7 +175,7 @@ int main(int argc, char **argv)
   const char*    user     = "readerOfTips";
   const char*    pass     = "tipReader123";
   const char*    db       = "HPCTips";
-  const char*    moduleNm = "tacc_tips"
+  const char*    moduleNm = "tacc_tips";
   const char*    hlp      = "(See \"module help %s\" for features or how to disable)";
   
 
