a9b35db168db8fd435599b3908cb19c44bc02274
Robert McLay <mclay@zaniahgroup.com>
2024-11-07 16:51:44 -0600

    removal mysql/my_global.h

--- src/showTip.C
+++ src/showTip.C
@@ -14,10 +14,6 @@
 #include "header.h"
 #include "version.h"
 
-//ifdef HAVE_MYSQL_MY_GLOBAL_H
-//  include "mysql/my_global.h"
-//endif
-
 #ifdef HAVE_MYSQL_MYSQL_H
 #  include "mysql/mysql.h"
 #endif
